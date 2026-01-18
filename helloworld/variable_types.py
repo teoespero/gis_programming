a8370871940ca142062d44e3fba1d4d950e04f32
Teo Espero <tespero@wgu.edu>
2026-01-17 22:00:29 -0800

    This is for demonstrating how variables are used in Python

---
+++ helloworld/variable_types.py
@@ -0,0 +1,11 @@
+# **********************************************************************************************
+# * Name:   variable_types.py
+# * Author: Teo Espero
+# *         MAS-GIT
+# * Date written:   01/17/2026
+# * Description:    This short Python script is used to demonstrate the ways data and variables
+# *                 are handled in the Python environment.
+# **********************************************************************************************
+
+num_points = 120    # integer
+print(num_points, type(num_points))
