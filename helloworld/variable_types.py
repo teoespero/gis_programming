6898cf77ac568370b4fdde97d91a1803e92c6a26
Teo Espero <tespero@wgu.edu>
2026-01-17 22:11:15 -0800

    This is for demonstrating how variables are used in Python

--- helloworld/variable_types.py
+++ helloworld/variable_types.py
@@ -7,5 +7,43 @@
 # *                 are handled in the Python environment.
 # **********************************************************************************************
 
-num_points = 120    # integer
+
+# -------------------------------------------------------
+# - Declare our data types
+# -------------------------------------------------------
+num_points = 120                # integer
+latitude = 35.6895              # float
+longitude = 139.6917            # float
+coordinate_system = "WGS 84"    # string
+is_georeferenced = True         # boolean
+coordinates = [                 # list
+    35.6895,
+    139.6917,
+]
+feature_attributes = {          # dictionaries
+    "name": "Mount Fuji",
+    "height_meters": 3776,
+    "type": "Stratovolcano",
+    "location": [35.3606, 138.7274],
+}
+
+
+
+
+print("Below are some of the data types that we will be using in the course.")
+print("---------------------------------------------------------------------\n")
+# -------------------------------------------------------
+# - Print the data and the data type used
+# -------------------------------------------------------
 print(num_points, type(num_points))
+print("lat", latitude, type(latitude))
+print("long", longitude, type(longitude))
+print(coordinate_system, type(coordinate_system))
+print(is_georeferenced, type(is_georeferenced))
+print(coordinates, type(coordinates))
+print(feature_attributes, type(feature_attributes))
+
+
+# -------------------------------------------------------
+# - EOF
+# -------------------------------------------------------
